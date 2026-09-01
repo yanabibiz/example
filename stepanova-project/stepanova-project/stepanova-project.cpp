7bd8c0b3e19e93e8e9bfbd25ce94b2da97ecd217
yanabibiz <yana.stepanova2007@gmail.com>
2026-09-01 13:34:12 +0300

    Добавлена функия if

--- stepanova-project/stepanova-project/stepanova-project.cpp
+++ stepanova-project/stepanova-project/stepanova-project.cpp
@@ -6,6 +6,13 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    int x;
+    std::cin >> x;
+
+    if (x != 0) {
+        std::cout << 10 / x;
+        return 0;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
